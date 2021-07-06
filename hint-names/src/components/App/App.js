1174dde6cc17c602b0335520893bb93ef08ca398
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-06 15:09:44 +0530

    functional and class based component

---
+++ hint-names/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+// Functional Component
+// function App(){
+//     return <h1>This is functional component</h1>;
+// }
+
+
+// Class Based Component
+class App extends React.Component {
+    
+    render(){
+        return <h1>This is class based component</h1>
+    }
+}
+
+export default App
