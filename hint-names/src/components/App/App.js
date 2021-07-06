3bf54782329280133d69778f19a4d8a6b3d0df42
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-06 17:06:07 +0530

    added animations

--- hint-names/src/components/App/App.js
+++ hint-names/src/components/App/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './App.css';
-import Header from '../../Header/Header';
+import Header from './Header/Header';
 import SearchBox from './SearchBox/SearchBox';
 
 class App extends React.Component {
     
     state = {
         headerText: "Hint Names",
-    }
+        headerExpanded: true,
+    };
+
+    handleInputChange = (inputText) => {
+        this.setState({ headerExpanded: !inputText
+        });
+    };
 
     render(){
         return (
             <div>
-                <Header headTitle={this.state.headerText} />
-                <SearchBox />
+                <Header headerExpanded={this.state.headerExpanded} headTitle={this.state.headerText} 
+                />
+                <SearchBox onInputChange={this.handleInputChange} />
             </div>
         )
     }
